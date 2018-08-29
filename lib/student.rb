cda13820aa16d71ba2f51f6f3610ee06c28e068f
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-08-29 19:28:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,12 @@ class Student
   
   def self.find_by_name(name)
     sql = <<-SQL
-        
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
     SQL
+    
+    DB[:conn].execute(sql, name)
   end 
 end
