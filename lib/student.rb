2b24cdce6f197ee5429d47dc16f6ad3b4dda09ac
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-08-29 19:48:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,10 @@ class Student
   end 
   
   def self.new_from_db(row)
-    student = self.new(db[0], db[1], db[2])
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
     student 
   end 
   
