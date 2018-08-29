a01316eaf6abd1d075d37edb72c00b24b1addf36
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-08-29 19:49:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   attr_accessor :name, :grade, :id
   attr_reader :id 
 
-  def initialize(id = nil, name:, grade:) 
+  def initialize(id = nil, name, grade) 
     self.name = name
     self.grade = grade
     self 
